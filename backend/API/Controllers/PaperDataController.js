177b303d71ffc4744b4fe92bd47f2817f1e065fc
DrCoco <mbk3801@autuni.ac.nz>
2018-05-09 15:01:17 +1200

    Backend working

--- backend/API/Controllers/PaperDataController.js
+++ backend/API/Controllers/PaperDataController.js
@@ -2,14 +2,14 @@
 var mongoose = require('mongoose');
 var PaperInfo = mongoose.model('PaperInfo');
 
-exports.processRequest = function(req, res) {
+exports.processRequest = function (req, res) {
     if (req.body.result.action == "name") {
-        getPaperName(req,res)
+        getPaperName(req, res);
     }
-    else if (req.body.result.action == "level")
-    {
-        getPaperLevel(req,res)
+    else if (req.body.result.action == "level") {
+        getPaperLevel(req, res)
     }
+
 };
 // Connect to database
 
@@ -18,43 +18,40 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://devtest:test@ds117540.mlab.com:17540/autpaperdata";
 
 
-function getPaperName(req,res)
-{
-let codeToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.paperCode ? req.body.result.parameters.paperCode : 'Unknown';
-MongoClient.connect(url, (err, client) => {
+function getPaperName(req, res) {
+    let codeToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.paperCode ? req.body.result.parameters.paperCode : 'Unknown';
+    MongoClient.connect(url, (err, client) => {
 
-    if(err) throw err;
-    
-    const db = client.db('autpaperdata');
-  
-    var query = {Paper_Code: codeToSearch};
-    db.collection('PaperInfo').find(query).toArray( (err, codeExists) => {
-  
-        if(err) throw err;
-        console.log(codeExists);
-        
-        if (codeExists) {
+        if (err) throw err;
 
-            console.log(codeExists[0].Paper_Name);
-            console.log(JSON.stringify(codeExists[0]));
-            console.log(JSON.stringify({
-                speech: codeExists[0].Paper_Name,
-                displayText: codeExists[0].Paper_Name,
-                source: 'paper name'
-            }))
-            var payload = JSON.stringify(codeExists[0]);
-            codeExists.
-            return payload.json({
-                speech: payload.Paper_Name,
-                displayText: payload.Paper_Name,
-                source: 'paper_name'
-            });
+        const db = client.db('autpaperdata');
 
-            
-        }
-        client.close();
-  
+        var query = { Paper_Code: codeToSearch };
+        db.collection('PaperInfo').find(query).toArray((err, codeExists) => {
+
+            if (err) throw err;
+            console.log(codeExists);
+
+            if (codeExists) {
+
+                console.log(codeExists[0].Paper_Name);
+                console.log(JSON.stringify(codeExists[0]));
+                console.log(JSON.stringify({
+                    speech: codeExists[0].Paper_Name,
+                    displayText: codeExists[0].Paper_Name,
+                    source: 'paper name'
+                })) 
+                return res.json({
+                    speech: codeExists[0].Paper_Name,
+                    displayText: codeExists[0].Paper_Name,
+                    source: 'paper name'
+                });
+
+
+            }
+            client.close();
+
+        })
+
     })
-  
-  })
 }
