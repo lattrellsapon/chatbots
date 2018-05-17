a62f40726fb5eba9959c0b5d515edda4ad84a340
DrCoco <mbk3801@autuni.ac.nz>
2018-05-18 01:00:23 +1200

    More front-end

---
+++ angular-app/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Message } from './models/message';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public message : Message;
+  public messages : Message[];
+
+
+  constructor(){
+    this.message = new Message('', 'assets/images/user.png');
+    this.messages = [
+      new Message('Welcome to BCIS Paper Helper', 'assets/images/bot.png', new Date())
+    ];
+  }
+}
