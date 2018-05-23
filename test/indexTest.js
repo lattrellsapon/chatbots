44eece95ff99f759aa4729609adad40b910e94a9
lattrellsapon <lattrellsapon@gmail.com>
2018-05-24 10:10:20 +1200

    Modified Travis

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,17 @@
+const chai = require('chai');
+const should = chai.should();
 const assert = require('chai').assert;
 
+const request = require('supertest');
+
 const index = require('../index');
+
+//body parser
+// const bodyParser = require('body-parser');
+// index.use(bodyParser.urlencoded(
+//     {
+//         extended: false
+//     }));
 
 describe('Index', function()
 {
@@ -8,4 +19,17 @@ describe('Index', function()
     {
         assert.equal(index(), 'undefined');
     });
+
+    it('Should be status 200', () => 
+    {
+        request(index)
+        .get('/')
+        .end((err, res) => {
+            res.status.should.equal(200);
+            done(err);
+        });
+    });
 });
+
+
+
